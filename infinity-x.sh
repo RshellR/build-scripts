f39a8b46e806a57401ac1530db0ae19dc8e368fe
RshellR <rshellr2@gmail.com>
2025-07-26 12:55:41 +0530

    Add variables:

--- infinity-x.sh
+++ infinity-x.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+source /home/admin/.bashrc
+
+cd /tmp/src/android/
+
+set -v
+
+# Variables
+DEVICE_BRANCH=lineage-23.0
+VENDOR_BRANCH=lineage-23.0
+XIAOMI_BRANCH=lineage-23.0
+REPO_URL="--depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault"
+
